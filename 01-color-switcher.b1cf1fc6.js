61656f2ad61ae12f34b1463e0d03e0e85fd90f9e
iryna-romashchenko <107789600+iryna-romashchenko@users.noreply.github.com>
2022-08-26 16:23:52 +0000

    Deploying to gh-pages from @ iryna-romashchenko/goit-js-hw-09@67a3c3a34c5730cce925df4cf1b679fe2c3e940b 🚀

---
+++ 01-color-switcher.b1cf1fc6.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]")};let n=null;function e(){t.btnStop.disabled=!0,t.btnStart.disabled=!1}t.btnStart.addEventListener("click",(function(){n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.btnStop.disabled=!1,t.btnStart.disabled=!0})),t.btnStop.addEventListener("click",(function(){clearInterval(n),e()})),e();
+//# sourceMappingURL=01-color-switcher.b1cf1fc6.js.map
