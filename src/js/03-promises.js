2109cc33793abf90411a1c3526e59446f9039d35
iryna-romashchenko <irromas@ukr.net>
2022-08-29 16:27:02 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,11 +1,3 @@
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
